489e39727b6cc0008f5d288f4b466e99b6774db8
Nayeem <mahfujurrahamannayeem@gmail.com>
2025-04-15 01:12:45 +0600

    Display Blog Data And Hashtags

--- src/Components/Blog/blog.jsx
+++ src/Components/Blog/blog.jsx
@@ -1,10 +1,28 @@
 import PropTypes from 'prop-types';
 
-const blog = ({blog}) => {
-    console.log(blog)
+const Blog = ({blog}) => {
+    const {title,cover,author,author_image,reading_time,posted_date,hashtags}=blog;
+
     return (
         <div>
-            
+            <div className='flex justify-between'>
+                <div className='flex '>
+                    <img className='w-14' src={author_image} alt="" />
+                    <div className='text-2xl ml-6'><h3>{author}</h3>
+                        <p>{posted_date}</p>
+                    </div>
+                </div>
+                <div>
+                    <span>{reading_time} min read </span>
+                </div>
+            </div>
+            <img  src={cover} alt={`Cover Picture of The Title ${title}`} />
+            <h2 className='text-4xl'>{title}</h2>
+            <p>
+                {
+                    hashtags.map(hash => <span><a href="">#{hash}</a></span>)
+                }
+            </p>
         </div>
     );
 };
@@ -13,4 +31,4 @@ const blog = ({blog}) => {
 Blog.propTypes = {
     blog : PropTypes.object.isRequired
 }
-export default blog;
+export default Blog;
